aa59fdbdfacb34c26ad9bae9bb915b6042a522da
Saad Ahmed <saadahmedez1@gmail.com>
2025-02-23 01:25:21 +0200

    Add Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  function Login() {
+    console.log(userName, password);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={(e) => e.preventDefault()} className="bg-gray-500 w-[25%] mx-auto mt-[100px] p-5 flex justify-center rounded-md">
+        <div className="w-full">
+          <label htmlFor="userName"></label>
+          <input onChange={(e) => setUserName(e.target.value)} className="block w-full m-auto rounded p-1 outline-none" type="text" autoFocus placeholder="Enter The userName" id="userName"></input>
+          <label htmlFor="password"></label>
+          <input onChange={(e) => setPassword(e.target.value)} className="block w-full mx-auto mt-3 rounded p-1 outline-none" typeof="password" placeholder="Enter The Password" id="password"></input>
+          <input onClick={Login} className="block mx-auto mt-5 w-full bg-green-500 text-white p-3 rounded-lg cursor-pointer" type="submit" value="Login"></input>
+        </div>
+      </form>
+    </>
   );
 }
 
